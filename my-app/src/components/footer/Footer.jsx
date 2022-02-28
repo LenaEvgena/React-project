8c8d2cd4984cad1a8aa9ab55544325119ea04e50
Elena Ostrouh <elena.ostrouh@intexsoft.by>
2022-02-28 11:28:14 +0300

    feat: add footer component

---
+++ my-app/src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import LogoTitle from '../logoTitle/LogoTitle';
+import './Footer.scss';
+
+const Footer = () => (
+  <footer className="footer">
+    <LogoTitle />
+  </footer>
+);
+
+export default Footer;
