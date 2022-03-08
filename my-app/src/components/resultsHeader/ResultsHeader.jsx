6f92a10363d0f952c620af9cac6b0c81e3cf6194
Alena Astravukh <helen151204@gmail.com>
2022-03-08 22:46:15 +0300

    feat: refactor results header

--- my-app/src/components/resultsHeader/ResultsHeader.jsx
+++ my-app/src/components/resultsHeader/ResultsHeader.jsx
@@ -1,35 +1,33 @@
 import React, { useEffect } from 'react';
-import { connect, useDispatch } from 'react-redux';
+import { connect } from 'react-redux';
 import './ResultsHeader.scss';
 import { getMoviesAPI } from '../../redux/api';
 import { sortMoviesAsync, filterGenreMoviesAsync, setCurrentPage } from '../../redux/actions';
 
-const SortResultsHeader = ({ currentPage, filter, sortType, keyword }) => {
-  const dispatch = useDispatch();
-
+const SortResultsHeader = ({ currentPage, filter, sortType, keyword, getMoviesAPI, sortMoviesAsync, filterGenreMoviesAsync, setCurrentPage }) => {
   useEffect(() => {
-    dispatch(setCurrentPage(1));
+    setCurrentPage(1);
   }, [filter])
 
   useEffect(() => {
     window.scroll(0, 0);
-    dispatch(getMoviesAPI(currentPage, sortType, filter, keyword));
+    getMoviesAPI(currentPage, sortType, filter, keyword);
   }, [currentPage, sortType, filter, keyword]);
 
   return (
     <div className="results__header">
       <div className="results__filter">
         <ul>
-          <li className={filter === 'all' ? 'active' : ''} onClick={() => dispatch(filterGenreMoviesAsync('all'))}>All</li>
-          <li className={filter === 'drama' ? 'active' : ''} onClick={() => dispatch(filterGenreMoviesAsync('drama'))}>Drama</li>
-          <li className={filter === 'melodrama' ? 'active' : ''} onClick={() => dispatch(filterGenreMoviesAsync('melodrama'))}>Melodrama</li>
-          <li className={filter === 'thriller' ? 'active' : ''} onClick={() => dispatch(filterGenreMoviesAsync('thriller'))}>Thriller</li>
-          <li className={filter === 'crime' ? 'active' : ''} onClick={() => dispatch(filterGenreMoviesAsync('crime'))}>Crime</li>
+          <li className={filter === 'all' ? 'active' : ''} onClick={() => filterGenreMoviesAsync('all')}>All</li>
+          <li className={filter === 'drama' ? 'active' : ''} onClick={() => filterGenreMoviesAsync('drama')}>Drama</li>
+          <li className={filter === 'melodrama' ? 'active' : ''} onClick={() => filterGenreMoviesAsync('melodrama')}>Melodrama</li>
+          <li className={filter === 'thriller' ? 'active' : ''} onClick={() => filterGenreMoviesAsync('thriller')}>Thriller</li>
+          <li className={filter === 'crime' ? 'active' : ''} onClick={() => filterGenreMoviesAsync('crime')}>Crime</li>
         </ul>
       </div>
       <div className="results__sort">
         <span>Sort by</span>
-        <select className="select" onClick={(e) => dispatch(sortMoviesAsync(e.target.value))}>
+        <select className="select" onClick={(e) => sortMoviesAsync(e.target.value)}>
           <option value="RATING">Rating</option>
           <option value="YEAR">Release date</option>
         </select>
@@ -45,11 +43,11 @@ const mapStateToProps = (state) => ({
   keyword: state.keyword,
 })
 
-const mapDispatchToProps = () => ({
-  getMoviesAPI,
-  sortMoviesAsync,
-  filterGenreMoviesAsync,
-  setCurrentPage,
+const mapDispatchToProps = (dispatch) => ({
+  getMoviesAPI: (currentPage, sortType, filter, keyword) => dispatch(getMoviesAPI(currentPage, sortType, filter, keyword)),
+  sortMoviesAsync: (e) => dispatch(sortMoviesAsync(e)),
+  filterGenreMoviesAsync: (v) => dispatch(filterGenreMoviesAsync(v)),
+  setCurrentPage: (v) => dispatch(setCurrentPage(v)),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(SortResultsHeader);
