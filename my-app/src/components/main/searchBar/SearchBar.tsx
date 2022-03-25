cfcb9d97671ac6cea88f443954961c4096f1ef39
Elena Ostrouh <elena.ostrouh@intexsoft.by>
2022-03-25 17:07:16 +0300

    feat: start editing formik

--- my-app/src/components/main/searchBar/SearchBar.tsx
+++ my-app/src/components/main/searchBar/SearchBar.tsx
@@ -5,12 +5,20 @@ import { useTypedSelector } from '../../../hooks/useTypedSelector';
 import classNames from 'classnames';
 import Button from '../../common/button/Button';
 import './SearchBar.scss';
+import { Formik, Form, Field, ErrorMessage, FormikHelpers } from 'formik';
+import * as Yup from 'yup';
 
 type PropsType = {
   isBusy: boolean,
   handleClick: () => void,
   handleResetClick: () => void,
 }
+
+// type SchemaType = {
+//   word: string,
+// }
+
+// let schema = Yup.string();
 
 const SearchBar: React.FC<PropsType> = ({ isBusy, handleClick, handleResetClick }) => {
   const dispatch = useDispatch();
@@ -21,11 +29,36 @@ const SearchBar: React.FC<PropsType> = ({ isBusy, handleClick, handleResetClick 
     dispatch(setMoviesKeyword(e.target.value));
   }
 
+  // const submit = (values: SchemaType, { setSubmitting }: FormikHelpers<SchemaType>) => {
+  //   handleClick();
+  // }
+
   return (
     <div className="search">
+
+      {/* <Formik
+        initialValues={{
+          word: '',
+        }}
+        validationSchema={schema}
+        onSubmit={submit}
+      >
+        {({ isSubmitting }) => (
+          <Form className="search__form">
+
+            <Field className="search__field" type="text" placeholder="What do you want to watch? Enter a keyword..." name="word" />
+            <ErrorMessage name="word" component="div" className="form__error" />
+
+            <Button className='search__button search__button-reset' type='button' handleClick={handleResetClick} />
+            <Button className={cls} type='submit' text='Search' />
+          </Form>
+        )}
+      </Formik> */}
+
+
       <input className="search__field" type="text" placeholder="What do you want to watch? Enter a keyword..." value={keyword} onChange={handleChange} />
       <Button className='search__button search__button-reset' type='button' handleClick={handleResetClick} />
-      <Button className={cls} type='button' text='Search' handleClick={handleClick} />
+      <Button className={cls} type='submit' text='Search' handleClick={handleClick} />
     </div>
   );
 }
